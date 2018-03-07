b22776557ee92f6e1ad9a05da6789956368d1713
minhduc <minhduc0322@gmail.com>
2018-03-07 15:43:06 +0700

    override save method

--- user/models.py
+++ user/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 
+from django.forms import ModelForm
+
 # Create your models here.
 class User(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    fullname = models.CharField(max_length=201)
+    fullname = models.CharField(max_length=201, default=0)
     password = models.CharField(max_length=100)
     birthday = models.DateField()
     hobbies = models.TextField()
     u_desc = models.TextField()
 
-    def __str__(self):
+    def save(self, *args, **kwargs):
+        self.getfullname()
+        super().save(*args, **kwargs)
+
+    def getfullname(self):
         self.fullname = self.firstname + " " + self.lastname
-        return self.fullname
 
 class Group(models.Model):
     GROUP_PERMISSION_CHOICES = (
